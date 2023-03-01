fd83207e12a15bdeb821de4d031d87f43c29eaa6
Patrícia Vasques <patriciahhhh@gmail.com>
2023-02-28 22:30:46 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var nome = prompt("Qual seu nome?");
+var idade = prompt("Qual sua idade?");
+var cidade = prompt("De qual cidade você é?");
+
+console.log ("nome:", nome, "idade:", idade, "cidade:", cidade);
